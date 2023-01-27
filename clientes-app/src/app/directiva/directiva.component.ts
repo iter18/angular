1880a0b69a631c80bfc6309b80e01b543b362b8f
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-01-27 16:15:13 -0600

    Se agrega ejemplo con directiva ngIf

--- clientes-app/src/app/directiva/directiva.component.ts
+++ clientes-app/src/app/directiva/directiva.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
 export class DirectivaComponent {
 
   listaCurso: string[] = ['TypeScript','JavaScript','Java SE','C#','PHP'];
+  habilitar: boolean = true;
+
   constructor(){
 
+  }
+
+  setHabilitar():void{
+    this.habilitar = this.habilitar == true ? false : true;
   }
 }
