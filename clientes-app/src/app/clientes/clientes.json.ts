ae26794eff54396359c6ec650a8997be2296a561
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-01-27 18:12:06 -0600

    Śe agrega archivo json para simular petición API Rest

---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,74 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES : Cliente[]= [
+    {
+      id : 1,
+      nombre : "José Antonio",
+      apellido : "Ojeda",
+      email : "ti0911342o@gmail.com",
+      createAt : "2023-01-01"
+    },
+    {
+      id : 2,
+      nombre : "José Aarón",
+      apellido : "Ojeda",
+      email : "aaron@gmail.com",
+      createAt : "2023-01-02"
+    },
+    {
+      id : 3,
+      nombre : "Viviana",
+      apellido : "Bonifacio",
+      email : "viviboes@gmail.com",
+      createAt : "2023-01-03"
+    },
+    {
+      id : 4,
+      nombre : "Luis Enrrique",
+      apellido : "Ojeda",
+      email : "lojeda@gmail.com",
+      createAt : "2023-01-04"
+    },
+    {
+      id : 5,
+      nombre : "Rebeca",
+      apellido : "Martínez",
+      email : "rebeO@gmail.com",
+      createAt : "2023-01-05"
+    },
+    {
+      id : 6,
+      nombre : "Axel",
+      apellido : "Rodes",
+      email : "arodes@gmail.com",
+      createAt : "2023-01-06"
+    },
+    {
+      id : 7,
+      nombre : "Omar",
+      apellido : "Bonifacio",
+      email : "nac_72ob@gmail.com",
+      createAt : "2023-01-07"
+    },
+    {
+      id : 8,
+      nombre : "Luis Manuel",
+      apellido : "García",
+      email : "lmga@gmail.com",
+      createAt : "2023-01-08"
+    },
+    {
+      id : 9,
+      nombre : "Julio Cesar",
+      apellido : "Dominguez",
+      email : "jdg@gmail.com",
+      createAt : "2023-01-09"
+    },
+    {
+      id : 2,
+      nombre : "Miguel Angel",
+      apellido : "Lerma",
+      email : "mlm@gmail.com",
+      createAt : "2023-01-10"
+    }
+  ]
