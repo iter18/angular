2717ac6e79c786760d08a21854df8b5aa48ec8d2
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-02-11 19:21:04 -0600

    Se agrega sweetAlert2 a proyecto

--- clientes-app/src/app/clientes/form.component.ts
+++ clientes-app/src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -29,7 +30,11 @@ export class FormComponent implements OnInit {
 
     console.log("Método Llamado");
     this.clienteServie.crear(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      cliente => {
+       
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo Cliente',`Cliente ${cliente.nombre} creado correctamente`,'success' )
+      }
     )
   }
 
