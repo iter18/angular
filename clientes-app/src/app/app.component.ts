15a4a571c625376c2ffc672e8fc44c6198fd9bd8
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-02-13 16:49:00 -0600

    Se agrega estilo a listado de cliente

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenido a Angular';
+  title = '';
   curso:string = 'Angular 15';
   persona:string = 'José Antonio';
 }
